28cbd7b0597922295e2c0648e859b5779af68a23
Sabareesh K <sabareesh@hipro.co.in>
2021-01-24 23:21:46 +0530

    Add more opening lines

--- app/opening_book.go
+++ app/opening_book.go
@@ -39,13 +39,6 @@ var book = []openingLine{
 		},
 		weight: 0,
 	},
-	openingLine{ // French Defense
-		moves: []UserMove{
-			UserMove{"e2", "e4"},
-			UserMove{"e7", "e6"},
-		},
-		weight: 0,
-	},
 	openingLine{ // Ruy Lopez Opening
 		moves: []UserMove{
 			UserMove{"e2", "e4"},
@@ -53,6 +46,88 @@ var book = []openingLine{
 			UserMove{"g1", "f3"},
 			UserMove{"b8", "c6"},
 			UserMove{"f1", "b5"},
+		},
+		weight: 0,
+	},
+	openingLine{ // Caro-Kann Defense
+		moves: []UserMove{
+			UserMove{"e2", "e4"},
+			UserMove{"c7", "c6"},
+		},
+		weight: 0,
+	},
+	openingLine{ // Italian Game
+		moves: []UserMove{
+			UserMove{"e2", "e4"},
+			UserMove{"e7", "e5"},
+			UserMove{"g1", "f3"},
+			UserMove{"b8", "c6"},
+			UserMove{"f1", "c4"},
+		},
+		weight: 0,
+	},
+	openingLine{ // Sicilian Defense: Closed
+		moves: []UserMove{
+			UserMove{"e2", "e4"},
+			UserMove{"c7", "c5"},
+			UserMove{"b1", "c3"},
+		},
+		weight: 0,
+	},
+	openingLine{ // Scandinavian Defense
+		moves: []UserMove{
+			UserMove{"e2", "e4"},
+			UserMove{"d7", "d5"},
+		},
+		weight: 0,
+	},
+	openingLine{ // Pirc Defense
+		moves: []UserMove{
+			UserMove{"e2", "e4"},
+			UserMove{"d7", "d6"},
+			UserMove{"d2", "d4"},
+			UserMove{"g8", "f6"},
+		},
+		weight: 0,
+	},
+	openingLine{ // Sicilian Defense: Alapin Variation
+		moves: []UserMove{
+			UserMove{"e2", "e4"},
+			UserMove{"c7", "c5"},
+			UserMove{"c2", "c3"},
+		},
+		weight: 0,
+	},
+	openingLine{ // Alekhine's Defense
+		moves: []UserMove{
+			UserMove{"e2", "e4"},
+			UserMove{"g8", "f6"},
+		},
+		weight: 0,
+	},
+	openingLine{ // King's Gambit
+		moves: []UserMove{
+			UserMove{"e2", "e4"},
+			UserMove{"e7", "e5"},
+			UserMove{"f2", "f4"},
+		},
+		weight: 0,
+	},
+	openingLine{ // Scotch Game
+		moves: []UserMove{
+			UserMove{"e2", "e4"},
+			UserMove{"e7", "e5"},
+			UserMove{"g1", "f3"},
+			UserMove{"b8", "c6"},
+			UserMove{"d2", "d4"},
+		},
+		weight: 0,
+	},
+	openingLine{ // Vienna Game
+		moves: []UserMove{
+			UserMove{"e2", "e4"},
+			UserMove{"e7", "e5"},
+			UserMove{"b1", "c3"},
 		},
 		weight: 0,
 	},
